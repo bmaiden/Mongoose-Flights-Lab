bf5c445d5a280fdf8acbee11b304fba3231a3c8b
bmaiden <b.maiden2012@yahoo.com>
2023-12-13 14:56:33 -0600

    adds MongoDB flights and Mongoose module with connection

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 require('dotenv').config();
+// connect to the database with AFTER the config vars are processed
+require('./config/database');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
