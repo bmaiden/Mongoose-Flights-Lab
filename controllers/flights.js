7dad77b683eef8d56f78eca028e1bdada26fbc22
bmaiden <b.maiden2012@yahoo.com>
2023-12-14 13:00:54 -0600

    adds styling and detail page

--- controllers/flights.js
+++ controllers/flights.js
@@ -2,19 +2,25 @@ const Flight = require('../models/flight')
 
 module.exports = {
     index,
+    show,
     new: newFlight,
     create
   };
 
   async function index(req, res) {
     const flights = await Flight.find({});
-    res.render('flights/index', { flights });
+    res.render('flights/index', { title: 'All Flights', flights });
+  }
+
+  async function show(req, res) {
+    const flight = await Flight.findById(req.params.id);
+    res.render('flights/show', { title: 'Flight Detail', flight });
   }
 
   function newFlight(req, res) {
     // We'll want to be able to render an  
     // errorMsg if the create action fails
-    res.render('flights/new', { errorMsg: '' });
+    res.render('flights/new', { title: 'Add Flight', errorMsg: '' });
   }
 
   async function create(req, res) {
