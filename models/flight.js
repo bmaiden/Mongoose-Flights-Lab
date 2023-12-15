a14f97465de9c1bf84feef50b9e4182f419cbf30
bmaiden <b.maiden2012@yahoo.com>
2023-12-15 15:17:39 -0600

    adds DEN as default with changes to the new Schema

--- models/flight.js
+++ models/flight.js
@@ -3,12 +3,24 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const flightSchema = new Schema({
-    airline: String,
-    airport: String,
-    flightNo: Number,
-    departs: Date
-}, {
-    timestamps: true
+    airline: {
+        type: String,
+        enum: ['American', 'Southwest', 'United']
+    },
+    airport: {
+        type: String,
+        enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'],
+        default: 'DEN'
+    },
+    flightNo: {
+        type: Number,
+        min: 10,
+        max: 9999
+    },
+    departs: {
+        type: Date,
+        default: new Date(new Date().setFullYear(new Date().getFullYear() + 1))
+    }
   });
 
   module.exports = mongoose.model('Flight', flightSchema);
